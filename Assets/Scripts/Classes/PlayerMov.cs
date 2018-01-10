b8e3cb91ad771e09cbed77039e64885c6c6f3465
felwoord <felwoord@hotmail.com>
2018-01-10 02:17:40 -0200

    1.0
    Basic Motion added

--- Assets/Scripts/Classes/PlayerMov.cs
+++ Assets/Scripts/Classes/PlayerMov.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Collections;
+using UnityEngine;
 
 namespace AssemblyCSharp
 {
 	public class PlayerMov
 	{
-		public PlayerMov ()
+		public float speed;
+		private Vector2 direction;
+	
+		public PlayerMov (float spd)
 		{
+			speed = spd;
+		}
+
+		public Vector2 SpeedCalc (Vector2 drct){
+			return (speed * drct.normalized);
 		}
 	}
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnCollisionEnter2D (Collision2D col){
+		if (col.gameObject.tag == "GuideBall") {
+			rb.velocity = new Vector2 (0, 0);
+		}
+	}
+}
