9be366c737e86ddf40016c4da6c2edd4721ecd22
Mephitzu <matheus.hos@puccampinas.edu.br>
2018-01-11 23:07:00 -0200

    1.0

---
+++ Assets/Scripts/StageScroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageScroller : MonoBehaviour {
+
+	private GameObject block;
+
+	void Start () {
+		block = GameObject.Find ("Block");
+
+	}
+	
+	void Update () {
+		
+	}
+}
